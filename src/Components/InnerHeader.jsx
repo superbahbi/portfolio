8b533f03c26fbfb106fabe85ad8dc5bd68accd17
Bobby Lee <superbahbi@users.noreply.github.com>
2022-06-18 06:20:24 -0700

    remove star and meteor styled components

--- src/Components/InnerHeader.jsx
+++ src/Components/InnerHeader.jsx
@@ -1,25 +1,10 @@
-import React from "react";
-import { Text, Stack, Heading, theme } from "@chakra-ui/react";
+import React, { Suspense, lazy } from "react";
+import { Text, Stack, Heading } from "@chakra-ui/react";
 import styled, { keyframes } from "styled-components";
+// import Meteor from "./Animation/Meteor";
+import Star from "./Animation/Star";
+const LazyMeteor = React.lazy(() => import("./Animation/Meteor"));
 
-const meteor = keyframes`
-0% {
-  opacity: 1;
-  margin-top: -300px;
-  margin-right: -300px;
-}
-12% {
-  opacity: 0;
-}
-15% {
-  margin-top: 300px;
-  margin-left: -600px;
-  opacity: 0;
-}
-100% {
-  opacity: 0;
-}
-`;
 const typewriter = (width) => keyframes`
   from {
     width: 0;
@@ -37,41 +22,6 @@ const blinkCursor = keyframes`
   }
 `;
 
-const Star = styled.div`
-  width: 1px;
-  height: 1px;
-  background: transparent;
-  box-shadow: ${(props) => props.starShadowBox};P
-`;
-const Meteor = styled.div`
-  position: absolute;
-  top: ${(props) => props.horizontal}px;
-  left: ${(props) => props.vertical * 1}%;
-  width: 300px;
-  height: 1px;
-  transform: rotate(-45deg); // diagonal line from top right to bottom left
-  background-image: linear-gradient(
-    to right,
-    rgba(255, 255, 255, 1),
-    rgba(255, 255, 255, 0)
-  );
-
-  animation: ${meteor} ${(props) => props.delay}s linear infinite;
-  @media (max-width: 768px) {
-    animation: ${meteor} ${(props) => props.delay + 5}s linear infinite;
-  }
-  &:before {
-    content: "";
-    position: absolute;
-    width: 4px;
-    height: 5px;
-    border-radius: 50%;
-    margin-top: -2px;
-    margin-left: -148px;
-    background: rgba(255, 255, 255, 0.7);
-    box-shadow: 0 0 15px 3px #fff;
-  }
-`;
 const InnerHeaderContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -100,28 +50,14 @@ const InnerHeaderText = styled(Heading)`
   overflow: hidden;
   font-weight: 300;
 `;
+
 const InnerHeader = ({ name, position }) => {
-  const numOfMeteors = new Array(15).fill();
-  const numOfStars = 500;
-  let starShadowBox = "";
-  for (let i = 1; i < numOfStars + 1; i += 1) {
-    let x = Math.floor(Math.random() * 2820 + 1);
-    let y = Math.floor(Math.random() * 1235 + 1);
-    starShadowBox += `${x}px ${y}px #fff${i === numOfStars ? ";" : ", "} `;
-  }
   return (
     <>
-      {numOfMeteors.map((_, i) => {
-        return (
-          <Meteor
-            key={i}
-            delay={Math.floor((Math.random() * 15 + 3) * 100) / 100}
-            vertical={Math.floor(Math.random() * 90 + 9)}
-            horizontal={Math.floor(Math.random() * 300 + 100)}
-          />
-        );
-      })}
-      <Star starShadowBox={starShadowBox} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <LazyMeteor amount={15} />
+      </Suspense>
+      <Star amount={500} />
       <InnerHeaderContainer>
         <Stack>
           <InnerHeaderText
