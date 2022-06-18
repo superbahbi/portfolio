a34bf77d1c9319af8d7648f84ec72761685432fa
Bobby Lee <superbahbi@users.noreply.github.com>
2022-06-17 17:56:59 -0700

    added random star

--- src/Components/InnerHeader.jsx
+++ src/Components/InnerHeader.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 import { Text, Stack, Heading } from "@chakra-ui/react";
-import styled, { keyframes } from "styled-components";
+import styled, { keyframes, css } from "styled-components";
+function createCSS() {
+  const num = 300;
+  let styles = "";
+  styles += "width: 1px;";
+  styles += "height: 1px;";
+  styles += "background: transparent;";
+  styles += "box-shadow: ";
+  for (let i = 1; i < num + 1; i += 1) {
+    let x = Math.floor(Math.random() * 1920 + 1);
+    let y = Math.floor(Math.random() * 1080 + 1);
+    $x: styles += `${x}px ${y}px #fff${i === num ? ";" : ", "} `;
+  }
+  return css`
+    ${styles}
+  `;
+}
+const Star = styled.div`
+  ${createCSS()};
+`;
 const typewriter = keyframes`
   from {
     width: 0;
@@ -19,7 +38,6 @@ const blinkCursor = keyframes`
 `;
 const InnerHeaderContainer = styled.div`
   display: flex;
-
   justify-content: center;
   align-items: center;
   text-align: center;
@@ -30,7 +48,7 @@ const InnerHeaderContainer = styled.div`
   margin-top: 2rem;
   .animation {
     animation: ${typewriter} 3.5s steps(13) 1s 1 normal both,
-      ${blinkCursor} 400ms steps(13) infinite normal;
+      ${blinkCursor} 900ms steps(13) infinite normal;
   }
 `;
 const InnerHeaderText = styled(Heading)`
@@ -41,15 +59,25 @@ const InnerHeaderText = styled(Heading)`
   font-weight: 300;
 `;
 const InnerHeader = ({ name, position }) => {
+  let shadow;
+  for (let i = 0; i < 1; i++) {
+    let x = Math.floor(Math.random() * (1920 - 1 + 1) + 1);
+    let y = Math.floor(Math.random() * (1080 - 1 + 1) + 1);
+    shadow += `${x}px ${y}px #fff, `;
+  }
+  console.log(shadow);
   return (
-    <InnerHeaderContainer>
-      <Stack>
-        <InnerHeaderText fontWeight="100" className="animation">
-          {name}
-        </InnerHeaderText>
-        <Text fontSize="2xl">{position}</Text>
-      </Stack>
-    </InnerHeaderContainer>
+    <>
+      <Star shadow={shadow} />
+      <InnerHeaderContainer>
+        <Stack>
+          <InnerHeaderText fontWeight="100" className="animation">
+            {name}
+          </InnerHeaderText>
+          <Text fontSize="2xl">{position}</Text>
+        </Stack>
+      </InnerHeaderContainer>
+    </>
   );
 };
 
