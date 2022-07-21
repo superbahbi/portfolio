ac8a37312f6731338ce154558409a799aaf08c2f
Bobby Lee <superbahbi@gmail.com>
2022-07-20 18:52:33 -0700

    added corkify demo link

--- src/Data/Projects.js
+++ src/Data/Projects.js
@@ -133,7 +133,7 @@ const projects = [
     projectDescription:
       "A wine flash store built in react native framework using firebase auth, real time database, and storage.",
     demoLink: "https://github.com/superbahbi/corkify",
-    codeLink: "https://github.com/superbahbi/corkify",
+    codeLink: "https://expo.dev/@superbahbi/Corkify",
   },
   {
     projectImg: portfolioImage,
