db035fa0f63f9027307683721dfc35a92611f6ae
Bobby Lee <superbahbi@gmail.com>
2023-01-14 11:43:21 -0800

    Remove modal

--- src/App.js
+++ src/App.js
@@ -5,18 +5,6 @@ import {
   Grid,
   Heading,
   Text,
-  useDisclosure,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  FormControl,
-  FormLabel,
-  Input,
-  Button,
 } from "@chakra-ui/react";
 import React from "react";
 import styled from "styled-components";
@@ -87,8 +75,6 @@ const TechBox = styled(Box)`
   }
 `;
 function App() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const initialRef = React.useRef(null);
   const finalRef = React.useRef(null);
   const resumeLink =
     "https://user.fm/files/v2-370050f28b0b6ed363ec702bb53264e9/Robert_Kugler_Resume.pdf";
@@ -147,33 +133,7 @@ function App() {
                 />
               );
             })}
-            <Button onClick={onOpen}>Open Modal</Button>
           </ContentGrid>
-          <Modal isOpen={isOpen} onClose={onClose} initialFocusRef={initialRef}>
-            <ModalOverlay />
-            <ModalContent>
-              <ModalHeader>Create your account</ModalHeader>
-              <ModalCloseButton />
-              <ModalBody pb={6}>
-                <FormControl>
-                  <FormLabel>First name</FormLabel>
-                  <Input ref={initialRef} placeholder="First name" />
-                </FormControl>
-
-                <FormControl mt={4}>
-                  <FormLabel>Last name</FormLabel>
-                  <Input placeholder="Last name" />
-                </FormControl>
-              </ModalBody>
-
-              <ModalFooter>
-                <Button colorScheme="blue" mr={3}>
-                  Save
-                </Button>
-                <Button onClick={onClose}>Cancel</Button>
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
         </Content>
         <Waves
           theme={theme}
